7e1e8f10596a9c3a13f62b36d48da7a345821417
Jesse <jessyvee1@gmail.com>
2023-08-05 11:28:05 +0300

    ProductsList Done

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,124 +1,122 @@
-// AddProduct.js
-
 import { useState, useEffect } from 'react';
 
 function AddProduct({ productId }) {
 
-  const [product, setProduct] = useState({
-    
-    name: '',
-    description: '',
-    image: '',
-    price: '',
-    quantity: ''
-  });
+    const [product, setProduct] = useState({
 
-  useEffect(() => {
-    // Fetch product data from API
-  }, [productId]);
-
-  const handleChange = (e) => {
-    setProduct({
-      ...product,
-      [e.target.name]: e.target.value
+        name: '',
+        description: '',
+        image: '',
+        price: '',
+        quantity: ''
     });
-  }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    useEffect(() => {
+        // Fetch product data from API
+    }, [productId]);
 
-    try {
-      const response = await fetch(`/api/products/${productId}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(product)
-      });
-
-      if (response.ok) {
-        console.log('Product updated successfully!');
-      }
-
-    } catch (error) {
-      console.log(error);
+    const handleChange = (e) => {
+        setProduct({
+            ...product,
+            [e.target.name]: e.target.value
+        });
     }
-  }
 
-  return (
-    <div className="addproduct-nav">
-         
-      <div className="container my-5">
-        <div className="col-md-8 mx-auto delete h-100">
-          <div
-            className="card shadow px-3 py-5"
-            style={{ width: "600px", backgroundColor: "#E8F9FD" }}
-          >
-            <h2 className="card-title text-center">Add Product</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="id">ID:</label>
-              <input
-                className="form-control"
-                type="text"
-                id="id"
-                placeholder="Product ID"
-                value={productId}
-                onChange={handleChange}
-              />
-              <label htmlFor="id">Name:</label>
-              <input
-                className="form-control"
-                type="text"
-                id="name"
-                placeholder="Product Name"
-                value={product.name}
-                onChange={handleChange}
-              />
-              <label htmlFor="id">Description:</label>
-              <input
-                className="form-control"
-                type="text"
-                id="description"
-                placeholder="description"
-                value={product.description}
-                onChange={handleChange}
-              />
-              <label htmlFor="id">Image:</label>
-              <input
-                className="form-control"
-                type="text"
-                id="image"
-                placeholder="phot_url"
-                value={product.image}
-                onChange={handleChange}
-              />
-              <label htmlFor="id">Price:</label>
-              <input
-                className="form-control"
-                type="integer"
-                id="price"
-                placeholder="value"
-                value={product.price}
-                onChange={handleChange}
-              />
-              <label htmlFor="id">Quantity:</label>
-              <input
-                className="form-control"
-                type="integer"
-                id="quantity"
-                placeholder="value"
-                value={product.quantity}
-                onChange={handleChange}
-              />
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-              <button className="btn btn-primary" type="submit">
-                ADD
-              </button>
-            </form>
-          </div>
+        try {
+            const response = await fetch(`/api/products/${productId}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(product)
+            });
+
+            if (response.ok) {
+                console.log('Product updated successfully!');
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="addproduct-nav">
+
+            <div className="container my-5">
+                <div className="col-md-8 mx-auto delete h-100">
+                    <div
+                        className="card shadow px-3 py-5"
+                        style={{ width: "600px", backgroundColor: "#E8F9FD" }}
+                    >
+                        <h2 className="card-title text-center">Add Product</h2>
+                        <form onSubmit={handleSubmit}>
+                            {/* <label htmlFor="id">ID:</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                id="id"
+                                placeholder="Product ID"
+                                value={productId}
+                                onChange={handleChange}
+                            /> */}
+                            <label htmlFor="id">Title:</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                id="name"
+                                placeholder="Product Title"
+                                value={product.name}
+                                onChange={handleChange}
+                            />
+                            <label htmlFor="id">Description:</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                id="description"
+                                placeholder="description"
+                                value={product.description}
+                                onChange={handleChange}
+                            />
+                            <label htmlFor="id">Image:</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                id="image"
+                                placeholder="phot_url"
+                                value={product.image}
+                                onChange={handleChange}
+                            />
+                            <label htmlFor="id">Price:</label>
+                            <input
+                                className="form-control"
+                                type="integer"
+                                id="price"
+                                placeholder="value"
+                                value={product.price}
+                                onChange={handleChange}
+                            />
+                            <label htmlFor="id">Quantity:</label>
+                            <input
+                                className="form-control"
+                                type="integer"
+                                id="quantity"
+                                placeholder="value"
+                                value={product.quantity}
+                                onChange={handleChange}
+                            />
+
+                            <button className="btn btn-primary" type="submit">
+                                ADD
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    
-  );
+
+    );
 };
 
 
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,11 +1,105 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
 
-function ProductsList(){
-    return (
-        <div>
-           
-        welcome to the products list page
+function ProductsList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch product list from API or simulate
+    const simulatedProducts = [
+      {
+        id: 1,
+        name: 'Product 1',
+        description: 'Description 1',
+        image: 'image1.jpg',
+        price: '10.00',
+        quantity: 5,
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        description: 'Description 2',
+        image: 'image2.jpg',
+        price: '20.00',
+        quantity: 8,
+      },
+      {
+        id: 3,
+        name: 'Product 3',
+        description: 'Description 3',
+        image: 'image3.jpg',
+        price: '15.00',
+        quantity: 12,
+      },
+      {
+        id: 4,
+        name: 'Product 4',
+        description: 'Description 4',
+        image: 'image4.jpg',
+        price: '25.00',
+        quantity: 3,
+      },
+      {
+        id: 5,
+        name: 'Product 5',
+        description: 'Description 5',
+        image: 'image5.jpg',
+        price: '18.00',
+        quantity: 7,
+      },
+    ];
+
+    setProducts(simulatedProducts);
+  }, []);
+
+  const handleEdit = (productId) => {
+    // Handle edit logic
+  };
+
+  const handleDelete = (productId) => {
+    // Handle delete logic
+  };
+
+  return (
+    <div className="ProductsList-nav">
+      <div className="container my-5">
+        <div className="col-md-8 mx-auto delete h-100">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="card shadow px-3 py-5 mb-3"
+              style={{ width: "600px", backgroundColor: "#E8F9FD" }}
+            >
+              <h2 className="card-title text-center">Product List</h2>
+              <form>
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder={`Product ${product.id}`}
+                  value={product.name}
+                  readOnly
+                />
+                {/* Add other input fields here */}
+                <button
+                  className="btn btn-primary mr-2"
+                  type="button"
+                  onClick={() => handleEdit(product.id)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  onClick={() => handleDelete(product.id)}
+                >
+                  Delete
+                </button>
+              </form>
+            </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
+
 export default ProductsList;
