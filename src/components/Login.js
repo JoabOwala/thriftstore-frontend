7cd8c7e2219ff98101e21d7cf1ee00c1ff82f078
Benah0987 <wanyoikebenayah77@gmail.com>
2023-08-07 20:24:34 +0300

    Good

--- src/components/Login.js
+++ src/components/Login.js
@@ -47,7 +47,7 @@ function Login() {
   };
 
   // Get the history object using the useHistory hook
-  const history = useHistory();
+
   return (
     <div className="container d-flex justify-content-center align-items-center min-vh-100">
       <div className="row border rounded-5 p-3 bg-white shadow box-area">
